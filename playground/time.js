855ab81d1bd9fa1cde8617595886cfa6e9695cc2
peterlxb <peterlxb@gmail.com>
2018-02-08 15:26:18 +0800

    Formatting time use Moment

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+// start time : Jan 1st 1970 00:00:00
+var moment = require('moment');
+
+// var date = moment();
+// console.log(date.format('MMM Do YYYY'));
+
+var time = moment();
+console.log(time.format('h:mm a'));
