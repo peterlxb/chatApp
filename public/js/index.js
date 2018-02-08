ba2a2bedc758438c0f1bdf4f6d817bea854a517d
peterlxb <peterlxb@gmail.com>
2018-02-08 16:09:14 +0800

    Add moustache js

--- public/js/index.js
+++ public/js/index.js
@@ -41,23 +41,27 @@ $(function () {
 
     socket.on('NewMessage', function (message) {
         var formattedTime = moment(message.createdAt).format('h:mm a');
-        
-        var li = jQuery('<li></li>');
-        li.text(`${message.from}: ${formattedTime}: ${message.text}`);
-        $("#messages").append(li);
+        var template = $('#message-template').html();
+        var html = Mustache.render(template,{
+            text: message.text,
+            from:message.from,
+            createdAt: formattedTime
+        });
 
+        $('#messages').append(html);
         console.log("newMessage", message);
     });
 
     socket.on('NewLocationMessage', function(message) {
         var formattedTime = moment(message.createdAt).format('h:mm a');
+        var template = $('#location-message-template').html();
+        var html = Mustache.render(template, {
+            url: message.url,
+            from: message.from,
+            createdAt: formattedTime
+        });
 
-        var li = jQuery('<li></li>');
-        var a = jQuery('<a target="_blank">My current location</a>');
-        li.text(`${message.from}: ${formattedTime} `);
-        a.attr('href',message.url);
-        li.append(a);
-        $("#messages").append(li);
+        $('#messages').append(html);
     });
 
     
